c927d2bacd8dfcb4b42002fea5e6f243ee059ac3
haidc24v7k009-a11y <haidc24v7k009@vlvh.ctu.edu.vn>
2026-04-20 18:51:58 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -8,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application" });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
